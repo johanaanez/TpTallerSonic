94d337f817441e546df3552e118adb43637054b8
johaanez <johana.anez@gmail.com>
2017-03-10 19:20:51 -0300

    first commit

---
+++ Debug/src/TpTallerSonic.d
@@ -0,0 +1,1 @@
+src/TpTallerSonic.d: ../src/TpTallerSonic.cpp
---
+++ src/TpTallerSonic.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : TpTallerSonic.cpp
+// Author      : Joha
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include </usr/include/SDL2/SDL.h>
+
+using namespace std;
+
+int main() {
+
+
+	cout << "!!!ello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
