ad86b45588dc5576bb926acace7a71964e7f5786
johaanez <johana.anez@gmail.com>
2017-03-25 23:49:03 -0300

    add deleted files

--- Debug/src/Debug/src/subdir.mk
+++ Debug/src/Debug/src/subdir.mk
@@ -3,22 +3,9 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-CPP_SRCS += \
-../src/TpTallerSonic.cpp 
-
-OBJS += \
-./src/TpTallerSonic.o 
-
-CPP_DEPS += \
-./src/TpTallerSonic.d 
+O_SRCS += \
+../src/Debug/src/TpTallerSonic.o 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.cpp
-	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
 
--- Debug/src/TpTallerSonic.d
+++
@@ -1,1 +0,0 @@
-src/TpTallerSonic.d: ../src/TpTallerSonic.cpp
